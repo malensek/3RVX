049901c8d852db7aa045d9f86fb516d223a2be7b
Matthew Malensek <matt@malensek.net>
2014-10-23 22:48:43 -0600

    Fix tile texture initialization

--- 3RVX/MeterWnd/Meters/HorizontalTile.h
+++ 3RVX/MeterWnd/Meters/HorizontalTile.h
@@ -6,10 +6,12 @@ class HorizontalTile : public Meter {
 public:
     HorizontalTile(std::wstring bitmapName, int x, int y, int units):
     Meter(bitmapName, x, y, units) {
+        _rect.Width = _bitmap->GetWidth();
+        _rect.Height = _bitmap->GetHeight();
+
         _texture = new Gdiplus::TextureBrush(_bitmap, Gdiplus::WrapModeTile,
             0, 0, Width(), Height());
         _shiftMat = new Gdiplus::Matrix(1, 0, 0, 1, (float) X(), (float) Y());
-
         _texture->SetTransform(_shiftMat);
     }
 
@@ -17,6 +19,6 @@ public:
     ~HorizontalTile();
 
 private:
-    Gdiplus::TextureBrush   *_texture;
-    Gdiplus::Matrix         *_shiftMat;
+    Gdiplus::TextureBrush *_texture;
+    Gdiplus::Matrix *_shiftMat;
 };

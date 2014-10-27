a5d1b00443d843c858f79c7ed4d5381f09e1545d
Matthew Malensek <matt@malensek.net>
2014-10-26 23:29:05 -0600

    Window positioning

--- 3RVX/VolumeOSD.cpp
+++ 3RVX/VolumeOSD.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 
+#include "Monitor.h"
 #include "Skin.h"
 
 void VolumeOSD::LoadSkin(std::wstring skinName) {
@@ -14,6 +15,13 @@ void VolumeOSD::LoadSkin(std::wstring skinName) {
     for each (Meter *m in meters) {
         _mWnd.AddMeter(m);
     }
+
+    _mWnd.Update();
+    HMONITOR monitor = Monitor::Default();
+    const int mWidth = Monitor::Width(monitor);
+    const int mHeight = Monitor::Height(monitor);
+    _mWnd.X(mWidth / 2 - _mWnd.Width() / 2);
+    _mWnd.Y(mHeight - _mWnd.Height() - 140);
 }
 
 void VolumeOSD::MeterLevels(float level) {

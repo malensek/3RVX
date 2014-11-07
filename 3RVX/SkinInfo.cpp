61b7554cd2dd518280c90fe6df07d2665de4bfd0
Matthew Malensek <matt@malensek.net>
2014-11-06 17:17:35 -0700

    Add SkinInfo class: general skin information
    This lets us grab skin information without pulling in Skin and its dependencies

---
+++ 3RVX/SkinInfo.cpp
@@ -0,0 +1,54 @@
+#include "SkinInfo.h"
+
+#include "Logger.h"
+#include "Settings.h"
+#include "StringUtils.h"
+
+SkinInfo::SkinInfo(std::wstring skinFile) :
+_skinFile(skinFile) {
+    CLOG(L"Loading skin XML: %s", _skinFile.c_str());
+
+    /* Remove the '/skin.xml' portion from the file name to get the dir name. */
+    std::wstring xmlName = std::wstring(SKIN_XML);
+    _skinDir = _skinFile.substr(0, _skinFile.length() - (xmlName.length() + 1));
+
+    std::string u8FileName = StringUtils::Narrow(_skinFile);
+    tinyxml2::XMLError result = _xml.LoadFile(u8FileName.c_str());
+    if (result != tinyxml2::XMLError::XML_SUCCESS) {
+        throw std::logic_error("Failed to read XML file!");
+    }
+
+    _root = _xml.GetDocument()->FirstChildElement("skin");
+    if (_root == NULL) {
+        throw std::runtime_error("Could not find root XML element");
+    }
+}
+
+std::wstring SkinInfo::Author() {
+    tinyxml2::XMLHandle xmlHandle(_root);
+    tinyxml2::XMLElement *author = xmlHandle
+        .FirstChildElement("info")
+        .FirstChildElement("author")
+        .ToElement();
+
+    const char *authorText;
+    if (author == NULL || (authorText = author->GetText()) == NULL) {
+        return L"";
+    }
+
+    return std::wstring(StringUtils::Widen(authorText));
+}
+
+std::wstring SkinInfo::URL() {
+    tinyxml2::XMLHandle xmlHandle(_root);
+    tinyxml2::XMLElement *site = xmlHandle
+        .FirstChildElement("info")
+        .FirstChildElement("website")
+        .ToElement();
+
+    const char *text;
+    if (site == NULL || (text = site->GetText()) == NULL) {
+        return L"";
+    }
+    return std::wstring(StringUtils::Widen(text));
+}
---
+++ 3RVX/SkinInfo.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Windows.h>
+#include <list>
+#include "TinyXml2\tinyxml2.h"
+
+#define SKINS_DIR L"Skins"
+#define SKIN_XML L"skin.xml"
+
+class SkinInfo {
+public:
+    SkinInfo(std::wstring skinName);
+    std::wstring Author();
+    std::wstring URL();
+
+protected:
+    std::wstring _skinFile;
+    std::wstring _skinDir;
+    tinyxml2::XMLDocument _xml;
+    tinyxml2::XMLElement *_root;
+};

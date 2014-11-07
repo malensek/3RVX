6e0ec10e29f56af28f89ce0979ad32849418b98e
Matthew Malensek <matt@malensek.net>
2014-11-06 17:18:15 -0700

    Clean up imports and move constructor to implementation

--- 3RVX/VolumeOSD.cpp
+++ 3RVX/VolumeOSD.cpp
@@ -9,6 +9,33 @@
 #define TIMER_ANIMIN 101
 #define TIMER_ANIMOUT 102
 
+VolumeOSD::VolumeOSD(HINSTANCE hInstance) :
+_mWnd(hInstance, L"3RVX-MasterVolumeOSD", L"3RVX-MasterVolumeOSD"),
+_fout(_mWnd) {
+
+    WNDCLASSEX wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = NULL;
+    wcex.lpfnWndProc = &VolumeOSD::StaticWndProc;
+    wcex.cbClsExtra = NULL;
+    wcex.cbWndExtra = NULL;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"3RVX-VolumeDispatcher";
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    RegisterClassEx(&wcex);
+    /* throw exception if failed? */
+
+    _hWnd = CreateWindowEx(
+        NULL, L"3RVX-VolumeDispatcher", L"3RVX-VolumeDispatcher",
+        NULL, NULL, NULL, NULL, NULL, HWND_MESSAGE, NULL, hInstance, this);
+}
+
 void VolumeOSD::LoadSkin(Skin *skin) {
     Gdiplus::Bitmap *bg = skin->OSDBgImg("volume");
     _mWnd.BackgroundImage(bg);
--- 3RVX/VolumeOSD.h
+++ 3RVX/VolumeOSD.h
@@ -1,40 +1,13 @@
 #pragma once
 
 #include "MeterWnd\Animations\FadeOut.h"
-#include "MeterWnd\Meters\MeterTypes.h"
 #include "MeterWnd\MeterWnd.h"
-#include "MeterWnd\Meter.h"
-#include "Skin.h"
+
+class Skin;
 
 class VolumeOSD {
 public:
-    VolumeOSD(HINSTANCE hInstance) :
-    _mWnd(hInstance, L"3RVX-MasterVolumeOSD", L"3RVX-MasterVolumeOSD"),
-    _fout(_mWnd) {
-
-        WNDCLASSEX wcex;
-
-        wcex.cbSize = sizeof(WNDCLASSEX);
-        wcex.style = NULL;
-        wcex.lpfnWndProc = &VolumeOSD::StaticWndProc;
-        wcex.cbClsExtra = NULL;
-        wcex.cbWndExtra = NULL;
-        wcex.hInstance = hInstance;
-        wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-        wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-        wcex.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
-        wcex.lpszMenuName = NULL;
-        wcex.lpszClassName = L"3RVX-VolumeDispatcher";
-        wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
-        RegisterClassEx(&wcex);
-        /* throw exception if failed? */
-
-        _hWnd = CreateWindowEx(
-            NULL, L"3RVX-VolumeDispatcher", L"3RVX-VolumeDispatcher",
-            NULL, NULL, NULL, NULL, NULL, HWND_MESSAGE, NULL, hInstance, this);
-    };
-
+    VolumeOSD(HINSTANCE hInstance);
     void LoadSkin(Skin *skin);
     void MeterLevels();
     void MeterLevels(float value);

ddd3016ee7d43d563198a11b2ae283f418703c7d
Matthew Malensek <matt@malensek.net>
2014-11-13 23:06:08 -0700

    Fix horizontalbar sizing bug

--- 3RVX/MeterWnd/Meters/HorizontalBar.h
+++ 3RVX/MeterWnd/Meters/HorizontalBar.h
@@ -4,13 +4,15 @@
 class HorizontalBar : public Meter {
 public:
     HorizontalBar(std::wstring bitmapName, int x, int y, int units,
-        bool reversed):
+        bool reversed = false):
         Meter(bitmapName, x, y, units),
         _reversed(reversed) {
+        _rect.Width = _bitmap->GetWidth();
+        _rect.Height = _bitmap->GetHeight();
     };
 
     virtual void Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics);
 
 private:
-    bool _reversed = false;
+    bool _reversed;
 };

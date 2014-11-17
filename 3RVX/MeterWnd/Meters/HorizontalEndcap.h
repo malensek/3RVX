8b6811dad349dba8465545014917e2751b473e7f
Matthew Malensek <matt@malensek.net>
2014-11-16 23:58:02 -0600

    Move constructor to implementation file

--- 3RVX/MeterWnd/Meters/HorizontalEndcap.h
+++ 3RVX/MeterWnd/Meters/HorizontalEndcap.h
@@ -4,43 +4,7 @@
 
 class HorizontalEndcap : public Meter {
 public:
-    HorizontalEndcap(std::wstring bitmapName, int x, int y, int units):
-        Meter(bitmapName, x, y, units),
-        _lMargin(-1),
-        _rMargin(-1) {
-
-        using namespace Gdiplus;
-
-        Color currentPixel;
-        ARGB searchColor = 0xFFFF00FF; /* magic pink (FF,00,FF) */
-        unsigned int width = _bitmap->GetWidth();
-
-        /* Scan across, looking for magic pink delineators */
-        for (unsigned int x = 0; x < width; ++x) {
-            _bitmap->GetPixel(x, 0, &currentPixel);
-            if (currentPixel.GetValue() == searchColor) {
-                if (_lMargin < 0) {
-                    _lMargin = x;
-                } else {
-                    _rMargin = x;
-                    break;
-                }
-            }
-        }
-
-        /* 1 extra px for left margin line */
-        _unitWidth = _rMargin - _lMargin - 1;
-
-        _lMarginRect.X = X();
-        _lMarginRect.Y = Y();
-        _lMarginRect.Width = _lMargin;
-        _lMarginRect.Height = Height();
-
-        _rMarginRect.Y = Y();
-        _rMarginRect.Width = _bitmap->GetWidth() - _rMargin + 1;
-        _rMarginRect.Height = Height();
-    };
-
+    HorizontalEndcap(std::wstring bitmapName, int x, int y, int units);
     void Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics);
     void Value(float value);
 

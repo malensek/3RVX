1386501c79adab47d18f913871c63be1d5efc206
Matthew Malensek <matt@malensek.net>
2014-11-18 23:13:45 -0600

    Move constructor to implementation file

--- 3RVX/MeterWnd/Meters/Text.h
+++ 3RVX/MeterWnd/Meters/Text.h
@@ -8,22 +8,7 @@ public:
     Text(int x, int y, int width, int height,
         Gdiplus::Font *font, Gdiplus::StringAlignment align,
         std::wstring color, byte transparency,
-        std::wstring formatString) :
-    Meter(x, y, 100),
-    _font(font->Clone()),
-    _formatString(formatString) {
-        _rect.Width = width;
-        _rect.Height = height;
-
-        _strFormat.SetAlignment(align);
-
-        unsigned long c = wcstol(color.c_str(), '\0', 16);
-        unsigned long a = transparency << 24;
-        _fontColor = new Gdiplus::SolidBrush(c | a);
-
-        _replaceIndex = _formatString.find(L"[[PERC]]");
-    }
-
+        std::wstring formatString);
     ~Text();
 
     virtual void Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics);

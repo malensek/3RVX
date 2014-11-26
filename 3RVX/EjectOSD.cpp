3f4d8571d25146de826b2ec18bf9c931561a83ca
Matthew Malensek <matt@malensek.net>
2014-11-25 23:20:13 -0700

    Extend base OSD class

--- 3RVX/EjectOSD.cpp
+++ 3RVX/EjectOSD.cpp
@@ -1,1 +1,6 @@
 #include "EjectOSD.h"
+
+EjectOSD::EjectOSD(HINSTANCE hInstance, Settings &settings) :
+OSD(hInstance, L"3RVX-EjectDispatcher", settings) {
+
+}
--- 3RVX/EjectOSD.h
+++ 3RVX/EjectOSD.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include <Windows.h>
+
+#include "OSD.h"
+#include "Settings.h"
+
+class EjectOSD : public OSD {
+public:
+    EjectOSD(HINSTANCE hInstance, Settings &settings);
+};

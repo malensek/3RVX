94965b182b3ba6062ed0ab527b15b8a6a5a9c793
Matthew Malensek <matt@malensek.net>
2014-11-29 22:35:44 -0700

    Use pointers for MeterWnd being animated

--- 3RVX/MeterWnd/Animation.h
+++ 3RVX/MeterWnd/Animation.h
@@ -4,6 +4,6 @@
 
 class Animation {
 public:
-    virtual bool Animate(MeterWnd &meterWnd) = 0;
-    virtual void Reset(MeterWnd &meterWnd) = 0;
+    virtual bool Animate(MeterWnd *meterWnd) = 0;
+    virtual void Reset(MeterWnd *meterWnd) = 0;
 };
--- 3RVX/MeterWnd/Animations/FadeOut.cpp
+++ 3RVX/MeterWnd/Animations/FadeOut.cpp
@@ -1,17 +1,17 @@
 #include "FadeOut.h"
 
-bool FadeOut::Animate(MeterWnd &meterWnd) {
-    byte current = meterWnd.Transparency();
+bool FadeOut::Animate(MeterWnd *meterWnd) {
+    byte current = meterWnd->Transparency();
     int newTrans = current - 5;
     if (newTrans < 0) {
         newTrans = 0;
         return true;
     }
 
-    meterWnd.Transparency(newTrans);
+    meterWnd->Transparency(newTrans);
     return false;
 }
 
-void FadeOut::Reset(MeterWnd &meterWnd) {
-    meterWnd.Transparency(255);
+void FadeOut::Reset(MeterWnd *meterWnd) {
+    meterWnd->Transparency(255);
 }

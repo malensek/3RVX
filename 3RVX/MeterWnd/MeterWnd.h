a176546baa42b82bd3de4ef67841b0114fa16550
Matthew Malensek <matt@malensek.net>
2014-11-29 22:37:14 -0700

    Remove constructor

--- 3RVX/MeterWnd/MeterWnd.h
+++ 3RVX/MeterWnd/MeterWnd.h
@@ -9,49 +9,9 @@
 
 class MeterWnd {
 public:
-    MeterWnd(HINSTANCE hInstance, LPCWSTR className, LPCWSTR title):
-    _hInstance(hInstance),
-    _className(className),
-    _title(title) {
-        WNDCLASSEX wcex;
-        wcex.cbSize = sizeof(WNDCLASSEX);
-        wcex.style = 0;
-        wcex.lpfnWndProc = &MeterWnd::StaticWndProc;
-        wcex.cbClsExtra = 0;
-        wcex.cbWndExtra = 0;
-        wcex.hInstance = _hInstance;
-        wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-        wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
-        wcex.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
-        wcex.lpszMenuName = NULL;
-        wcex.lpszClassName = _className;
-        wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
-        if (!RegisterClassEx(&wcex)) {
-            /* throw exception */
-        }
-
-        _location.x = 0;
-        _location.y = 0;
-        _size.cx = 1;
-        _size.cy = 1;
-        _transparency = 255;
-
-        _hWnd = CreateWindowEx(
-            WS_EX_TOOLWINDOW |
-            WS_EX_LAYERED |
-            WS_EX_NOACTIVATE |
-            WS_EX_TOPMOST |
-            WS_EX_TRANSPARENT,
-            _className, _title,
-            WS_POPUP, CW_USEDEFAULT, CW_USEDEFAULT,
-            _size.cx, _size.cy,
-            NULL, NULL, _hInstance, this);
-
-        if (_hWnd == NULL) {
-            /* throw exception */
-        }
-    };
+    MeterWnd(HINSTANCE hInstance, LPCWSTR className, LPCWSTR title,
+        Animation *hideAnimation = NULL, Animation *showAnimation = NULL,
+        int visibleDuration = 0);
 
 	void Update();
 

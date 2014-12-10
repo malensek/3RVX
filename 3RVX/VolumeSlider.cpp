b3bdc44dbc101c1045c24ffd52e2c7b804f62d6d
Matthew Malensek <matt@malensek.net>
2014-12-09 23:37:41 -0700

    Initial volume slider implementation

--- 3RVX/VolumeSlider.cpp
+++ 3RVX/VolumeSlider.cpp
@@ -1,1 +1,25 @@
 #include "VolumeSlider.h"
+
+#include "Skin.h"
+
+VolumeSlider::VolumeSlider(HINSTANCE hInstance, Settings &settings) :
+OSD(hInstance, L"3RVX-VolumeSlider", settings),
+_mWnd(hInstance, L"3RVX-VolumeSliderWnd", L"3RVX Volume Slider") {
+
+    std::wstring skinXML = settings.SkinXML();
+    Skin skin(skinXML);
+
+    long styles = GetWindowLongPtr(_mWnd.HWnd(), GWL_EXSTYLE);
+    styles &= ~(WS_EX_NOACTIVATE | WS_EX_TRANSPARENT);
+    SetWindowLongPtr(_mWnd.HWnd(), GWL_EXSTYLE, styles);
+
+    Gdiplus::Bitmap *bg = skin.ControllerBgImg("volume");
+    _mWnd.BackgroundImage(bg);
+    _mWnd.Update();
+    _mWnd.Show();
+
+}
+
+void VolumeSlider::Hide() {
+    _mWnd.Hide(false);
+}
--- 3RVX/VolumeSlider.h
+++ 3RVX/VolumeSlider.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "OSD\OSD.h"
+#include "MeterWnd\MeterWnd.h"
+
+class VolumeSlider : public OSD {
+public:
+    VolumeSlider(HINSTANCE hInstance, Settings &settings);
+
+    void Hide();
+
+private:
+    MeterWnd _mWnd;
+
+};

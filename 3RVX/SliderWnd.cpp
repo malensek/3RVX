4424dc33bd3556606d5897697f136802222c2b26
Matthew Malensek <matt@malensek.net>
2014-12-11 23:16:43 -0700

    Set window styles on construction

--- 3RVX/SliderWnd.cpp
+++ 3RVX/SliderWnd.cpp
@@ -2,6 +2,7 @@
 
 SliderWnd::SliderWnd(HINSTANCE hInstance, LPCWSTR className, LPCWSTR title) :
 MeterWnd(hInstance, className, title) {
-
-
+    long styles = GetWindowLongPtr(_hWnd, GWL_EXSTYLE);
+    styles &= ~(WS_EX_NOACTIVATE | WS_EX_TRANSPARENT);
+    SetWindowLongPtr(_hWnd, GWL_EXSTYLE, styles);
 }
--- 3RVX/SliderWnd.h
+++ 3RVX/SliderWnd.h
@@ -8,5 +8,4 @@ public:
 
 private:
 
-
 };

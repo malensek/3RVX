0d2d0ba298719dfa713dbbe944c5e08117082ee6
Matthew Malensek <matt@malensek.net>
2014-12-15 19:47:15 -0700

    Add class to track the slider control knob

---
+++ 3RVX/Slider/SliderKnob.cpp
@@ -0,0 +1,52 @@
+#include "SliderKnob.h"
+
+SliderKnob::SliderKnob(std::wstring bitmapName,
+    int x, int y, int width, int height) :
+Meter(bitmapName, x, y, 1),
+_trackX(x),
+_trackY(y),
+_trackWidth(width),
+_trackHeight(height) {
+
+}
+
+Gdiplus::Rect SliderKnob::Location() {
+    return _rect;
+}
+
+void SliderKnob::Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics) {
+    graphics->DrawImage(_bitmap, _rect,
+        0, 0, Width(), _trackHeight, Gdiplus::UnitPixel);
+}
+
+int SliderKnob::TrackX() const {
+    return _trackX;
+}
+
+int SliderKnob::TrackY() const {
+    return _trackY;
+}
+
+int SliderKnob::TrackWidth() const {
+    return _trackWidth;
+}
+
+int SliderKnob::TrackHeight() const {
+    return _trackHeight;
+}
+
+int SliderKnob::X() const {
+    return Meter::X();
+}
+
+int SliderKnob::Y() const {
+    return Meter::Y();
+}
+
+void SliderKnob::X(int x) {
+    _rect.X = x;
+}
+
+void SliderKnob::Y(int y) {
+    _rect.Y = y;
+}
---
+++ 3RVX/Slider/SliderKnob.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <windows.h>
+#include <gdiplus.h>
+#pragma comment(lib, "gdiplus.lib")
+
+#include "..\MeterWnd\Meter.h"
+
+class SliderKnob : public Meter {
+public:
+    SliderKnob(std::wstring bitmapName, int x, int y, int width, int height);
+    virtual void Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics);
+    Gdiplus::Rect Location();
+
+    virtual int X() const;
+    virtual int Y() const;
+    void X(int x);
+    void Y(int y);
+
+    int TrackX() const;
+    int TrackY() const;
+    int TrackWidth() const;
+    int TrackHeight() const;
+
+private:
+    int _trackX;
+    int _trackY;
+    int _trackWidth;
+    int _trackHeight;
+};

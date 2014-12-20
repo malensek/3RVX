d1fffbf8219934586f8f0df84a07d8132bc236c8
Matthew Malensek <matt@malensek.net>
2014-12-19 23:35:55 -0700

    Reapplying WS_EX_TOPMOST is redundant

--- 3RVX/Slider/SliderWnd.cpp
+++ 3RVX/Slider/SliderWnd.cpp
@@ -9,7 +9,7 @@ MeterWnd(className, title, hInstance),
 _dragging(false) {
     long styles = GetWindowLongPtr(_hWnd, GWL_EXSTYLE);
     styles &= ~(WS_EX_NOACTIVATE | WS_EX_TRANSPARENT);
-    SetWindowLongPtr(_hWnd, GWL_EXSTYLE, styles | WS_EX_TOPMOST);
+    SetWindowLongPtr(_hWnd, GWL_EXSTYLE, styles);
 }
 
 void SliderWnd::Show() {

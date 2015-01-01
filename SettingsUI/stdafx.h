76abcc2a78ab842af28cb113fb18f8500e208c32
Matthew Malensek <matt@malensek.net>
2014-12-31 19:58:50 -0700

    More cleanup

--- SettingsUI/stdafx.h
+++ SettingsUI/stdafx.h
@@ -1,12 +1,8 @@
-
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently,
-// but are changed infrequently
-
 #pragma once
 
 #ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+/* Exclude rarely-used stuff from Windows headers */
+#define VC_EXTRALEAN
 #endif
 
 #include "targetver.h"
@@ -16,12 +12,8 @@
 // turns off MFC's hiding of some common and often safely ignored warning messages
 #define _AFX_ALL_WARNINGS
 
-#include <afxwin.h>         // MFC core and standard components
-#include <afxext.h>         // MFC extensions
-
-
-
-
+#include <afxwin.h>
+#include <afxext.h>
 
 #ifndef _AFX_NO_OLE_SUPPORT
 #include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
@@ -32,14 +24,6 @@
 
 #include <afxcontrolbars.h>     // MFC support for ribbons and control bars
 #include <afxdlgs.h>
-
-
-
-
-
-
-
-
 
 #ifdef _UNICODE
 #if defined _M_IX86

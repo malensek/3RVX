1e850b4edfe29ed6b3f1250d2c6054bf3ec77702
Matthew Malensek <matt@malensek.net>
2015-01-02 19:04:20 -0700

    Remove obsolete enums

--- 3RVX/MeterWnd/Meter.h
+++ 3RVX/MeterWnd/Meter.h
@@ -12,9 +12,6 @@ public:
     Meter(std::wstring bitmapName, int x, int y, int units);
     Meter(int x, int y, int units);
 
-    enum TextAlignment { Left, Right, Center };
-    enum TextStyle { Bold = 1, Italic = 2, UnderLine = 4, Strike = 8 };
-
     virtual void Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics) = 0;
     virtual bool Dirty();
 

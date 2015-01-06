a3bb84d24ce02ee4764b4f2663a5991d16c1851c
Matthew Malensek <matt@malensek.net>
2015-01-05 22:00:07 -0700

    Remove old test code

--- SettingsUI/SettingsUI.h
+++ SettingsUI/SettingsUI.h
@@ -6,26 +6,13 @@
 
 #include "resource.h"		// main symbols
 
-#include <string>
-
-#include "../3RVX/TinyXml2/tinyxml2.h"
 #define CHECKED(checkbox) { (checkbox.GetCheck() == 1) ? true : false }
 
 class CSettingsUIApp : public CWinApp {
 public:
     CSettingsUIApp();
     virtual BOOL InitInstance();
-
-    static void LoadCheckbox(
-        tinyxml2::XMLElement *xmlRoot, std::string elementName, CButton *check);
-
     DECLARE_MESSAGE_MAP()
-
-private:
-    tinyxml2::XMLDocument _xml;
-    tinyxml2::XMLElement *_root;
-
-    void LoadSettings();
 };
 
 extern CSettingsUIApp theApp;

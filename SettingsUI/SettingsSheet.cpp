911b52bb445358bbc5b574e0d919dba9acd38408
Matthew Malensek <matt@malensek.net>
2015-02-21 22:47:59 -0700

    Raise window to foreground if another instance was launched

--- SettingsUI/SettingsSheet.cpp
+++ SettingsUI/SettingsSheet.cpp
@@ -22,5 +22,12 @@ BOOL SettingsSheet::OnInitDialog() {
     return TRUE;
 }
 
+LRESULT SettingsSheet::WindowProc(UINT message, WPARAM wParam, LPARAM lParam) {
+    if (message == WM_3RVX_SETTINGSCTRL) {
+        SetForegroundWindow();
+    }
+    return CPropertySheet::WindowProc(message, wParam, lParam);
+}
+
 BEGIN_MESSAGE_MAP(SettingsSheet, CPropertySheet)
 END_MESSAGE_MAP()

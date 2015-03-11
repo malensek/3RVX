e779434f4345082703e2f98f401186407409b449
Matthew Malensek <matt@malensek.net>
2015-03-11 14:05:30 -0600

    Add configurable speed to Animation constructor

--- 3RVX/MeterWnd/Animation.h
+++ 3RVX/MeterWnd/Animation.h
@@ -4,6 +4,16 @@ class MeterWnd;
 
 class Animation {
 public:
+    Animation(int speed) :
+        _speed(speed) {
+
+    }
+
     virtual bool Animate(MeterWnd *meterWnd) = 0;
     virtual void Reset(MeterWnd *meterWnd) = 0;
+
+    virtual int UpdateInterval() = 0;
+
+protected:
+    int _speed;
 };

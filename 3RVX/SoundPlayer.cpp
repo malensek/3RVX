0e96dcabee4ab0e4c6e365d8f50d59920dcf6e43
Matthew Malensek <matt@malensek.net>
2015-03-12 17:52:58 -0600

    Experiment with multithreaded sound player

--- 3RVX/SoundPlayer.cpp
+++ 3RVX/SoundPlayer.cpp
@@ -1,7 +1,9 @@
 #include "SoundPlayer.h"
 
 #include <Windows.h>
+#include <chrono>
 #include <mmsystem.h>
+#include <mutex>
 #include <fstream>
 
 #define PLAY_SYNC SND_MEMORY
@@ -21,6 +23,8 @@ SoundPlayer::SoundPlayer(std::wstring filePath) {
     _sound = new char[(unsigned int) chars];
     file.read(_sound, chars);
     file.close();
+
+    //_thread = std::thread(&SoundPlayer::PlayerThread, this);
 }
 
 SoundPlayer::~SoundPlayer() {
@@ -31,6 +35,19 @@ SoundPlayer::~SoundPlayer() {
 }
 
 void SoundPlayer::Play(bool async) {
+    //_cv.notify_all();
     PlaySound(NULL, 0, 0);
-    PlaySound((LPCWSTR) _sound, NULL, (async ? PLAY_ASYNC : PLAY_SYNC));
+    PlaySound((LPCWSTR) _sound, NULL, SND_MEMORY | SND_NOSTOP);
+}
+
+void SoundPlayer::PlayerThread() {
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+    while (true) {
+        _cv.wait(lock);
+        //PlaySound(NULL, 0, 0);
+        PlaySound((LPCWSTR) _sound, NULL, SND_MEMORY | SND_NOSTOP);
+        std::this_thread::sleep_for(std::chrono::milliseconds(75));
+    }
+
 }
--- 3RVX/SoundPlayer.h
+++ 3RVX/SoundPlayer.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <condition_variable>
 #include <string>
+#include <thread>
 
 class SoundPlayer {
 public:
@@ -10,5 +12,9 @@ public:
     void Play(bool async = true);
 
 private:
+    std::condition_variable _cv;
     char *_sound;
+    std::thread _thread;
+
+    void PlayerThread();
 };

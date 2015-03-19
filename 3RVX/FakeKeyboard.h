d33136521329fa51f9f3e88e1887efb8d02b2e23
Matthew Malensek <matt@malensek.net>
2015-03-18 19:50:54 -0600

    Add class for simulating keyboard input

---
+++ 3RVX/FakeKeyboard.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Windows.h>
+#include "Logger.h"
+
+class FakeKeyboard {
+public:
+    static void SimulateKeypress(unsigned short vk) {
+        INPUT input = { 0 };
+        input.type = INPUT_KEYBOARD;
+        input.ki.wVk = vk;
+        input.ki.wScan = 0;
+        input.ki.dwFlags = 0;
+        input.ki.time = 0;
+        input.ki.dwExtraInfo = GetMessageExtraInfo();
+
+        /* key down: */
+        SendInput(1, &input, sizeof(INPUT));
+
+        /* key up: */
+        input.ki.dwFlags = KEYEVENTF_KEYUP;
+        SendInput(1, &input, sizeof(INPUT));
+    }
+};

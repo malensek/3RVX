2db95818d4deca2050052005e2791fe7937bc452
Matthew Malensek <matt@malensek.net>
2015-03-27 23:19:46 -0600

    Add documentation

--- 3RVX/MeterWnd/Meter.h
+++ 3RVX/MeterWnd/Meter.h
@@ -13,15 +13,35 @@ public:
     Meter(int x, int y, int units);
     ~Meter();
 
+    /// <summary>
+    /// Draws the current meter state onto the specified buffer.
+    /// </summary>
     virtual void Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics) = 0;
+    /// <summary>
+    /// Reports whether or not the meter's state has changed since the last
+    /// time it was drawn. If a meter is dirty, it should be redrawn.
+    /// </summary>
     virtual bool Dirty();
 
+    /// <summary>Retrieves the meter's current value (0 - 1.0)</summary>
     virtual float Value() const;
+    /// <summary>Sets the meter value (0 - 1.0)</summary>
     virtual void Value(float value);
 
+    /// <summary>
+    /// Retrieves the maximum number of units shown by this meter.
+    /// </summary>
     virtual int Units() const;
+    /// <summary>
+    /// Calculates the current number of units the meter should display.
+    /// </summary>
     virtual int CalcUnits();
 
+    /// <summary>
+    /// Retrieves the current meter state as a string. This includes
+    /// information such as the maximum number of units supported by
+    /// the meter, current value, etc.
+    /// </summary>
     virtual std::wstring ToString();
 
 protected:
@@ -29,6 +49,11 @@ protected:
     Gdiplus::Bitmap *_bitmap;
     Gdiplus::Rect _rect;
 
+    /// <summary>
+    /// Updates state variables after a draw operation. This helps distinguish
+    /// between a meter whose value has been changed, and a meter that has
+    /// been drawn with a new value.
+    /// </summary>
     void UpdateDrawnValues();
 
 private:

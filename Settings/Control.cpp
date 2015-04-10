786b6ef1b94ddac69a802ef9b64e37fda9434abd
Matthew Malensek <matt@malensek.net>
2015-04-09 18:51:56 -0600

    Add Control class to encapsulate window controls

---
+++ Settings/Control.cpp
@@ -0,0 +1,43 @@
+#include "Control.h"
+
+#include <sstream>
+
+Control::Control(int id, HWND parent) :
+_id(id),
+_parent(parent) {
+    _hWnd = GetDlgItem(parent, id);
+}
+
+bool Control::Text(std::wstring text) {
+    return SetDlgItemText(_parent, _id, text.c_str()) == TRUE;
+}
+
+bool Control::Text(int value) {
+    return Text(std::to_wstring(value));
+}
+
+std::wstring Control::Text() {
+    wchar_t text[MAX_EDITSTR];
+    GetDlgItemText(_parent, _id, text, MAX_EDITSTR);
+    return std::wstring(text);
+}
+
+int Control::TextAsInt() {
+    std::wstring str = Text();
+    int num;
+    std::wistringstream wistr(str);
+    wistr >> num;
+    return num;
+}
+
+void Control::AddWindowExStyle(long exStyle) {
+    long exs = GetWindowLongPtr(_hWnd, GWL_EXSTYLE);
+    exs |= exStyle;
+    SetWindowLongPtr(_hWnd, GWL_EXSTYLE, exs);
+}
+
+RECT Control::Dimensions() {
+    RECT r;
+    GetWindowRect(_hWnd, &r);
+    return r;
+}
---
+++ Settings/Control.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Windows.h>
+#include <string>
+
+class Control {
+public:
+    Control(int id, HWND parent);
+    ~Control();
+
+    RECT Dimensions();
+
+    void Enable();
+    void Disable();
+    void Enabled();
+    void Enabled(int id, bool enabled);
+
+    std::wstring Text();
+    int TextAsInt();
+    bool Text(std::wstring text);
+    bool Text(int value);
+
+    void WindowExStyle();
+    void WindowExStyle(long exStyle);
+    void AddWindowExStyle(long exStyle);
+    void RemoveWindowExStyle(long exStyle);
+
+protected:
+    int _id;
+    HWND _hWnd;
+    HWND _parent;
+
+protected:
+    static const int MAX_EDITSTR = 0x4000;
+};

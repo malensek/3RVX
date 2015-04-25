fadea23fe936030350e4dcf7a819f75af383e6be
Matthew Malensek <matt@malensek.net>
2015-04-24 18:07:12 -0600

    Create HotkeyPrompt window

--- Settings/HotkeyPrompt.cpp
+++ Settings/HotkeyPrompt.cpp
@@ -1,1 +1,64 @@
 #include "HotkeyPrompt.h"
+
+#include "../3RVX/Error.h"
+#include "resource.h"
+
+HotkeyPrompt::HotkeyPrompt(HINSTANCE hInstance, std::wstring className) {
+    if (hInstance == NULL) {
+        hInstance = (HINSTANCE) GetModuleHandle(NULL);
+    }
+    _hInstance = hInstance;
+
+    const wchar_t *cls = className.c_str();
+
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = NULL;
+    wcex.lpfnWndProc = &HotkeyPrompt::StaticWndProc;
+    wcex.cbClsExtra = NULL;
+    wcex.cbWndExtra = NULL;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH) (COLOR_WINDOW);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = cls;
+    wcex.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wcex)) {
+        throw SYSERR_REGISTERCLASS;
+    }
+
+    _hWnd = CreateWindowEx(NULL, cls, cls, NULL, NULL, NULL,
+        NULL, NULL, NULL, NULL, hInstance, this);
+
+    if (_hWnd == NULL) {
+        throw SYSERR_CREATEWINDOW;
+    }
+}
+
+void HotkeyPrompt::Show() {
+    DialogBox(_hInstance, MAKEINTRESOURCE(IDD_HOTKEYPROMPT), _hWnd, NULL);
+}
+
+LRESULT CALLBACK HotkeyPrompt::StaticWndProc(
+        HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    HotkeyPrompt *prompt;
+
+    if (message == WM_CREATE) {
+        prompt = (HotkeyPrompt *) ((LPCREATESTRUCT) lParam)->lpCreateParams;
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR) prompt);
+    } else {
+        prompt = (HotkeyPrompt *) GetWindowLongPtr(hWnd, GWLP_USERDATA);
+        if (!prompt) {
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+
+    return prompt->WndProc(hWnd, message, wParam, lParam);
+}
+
+LRESULT HotkeyPrompt::WndProc(
+        HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
--- Settings/HotkeyPrompt.h
+++ Settings/HotkeyPrompt.h
@@ -1,5 +1,21 @@
 #pragma once
 
-class HotkeyPrompt {
+#include <Windows.h>
+#include <string>
 
+class HotkeyPrompt {
+public:
+    HotkeyPrompt(HINSTANCE hInstance = NULL,
+        std::wstring className = L"3RVX Hotkey Input Dialog");
+
+    void Show();
+
+protected:
+    HINSTANCE _hInstance;
+    HWND _hWnd;
+
+    static LRESULT CALLBACK StaticWndProc(HWND hWnd, UINT message,
+        WPARAM wParam, LPARAM lParam);
+    virtual LRESULT WndProc(HWND hWnd, UINT message,
+        WPARAM wParam, LPARAM lParam);
 };

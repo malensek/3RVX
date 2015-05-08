72f40bd870b690a8ac9516224fa3027b944b0d9c
Matthew Malensek <matt@malensek.net>
2015-05-07 23:50:35 -0600

    Begin implementing the translation service

--- 3RVX/LanguageTranslator.cpp
+++ 3RVX/LanguageTranslator.cpp
@@ -1,5 +1,25 @@
 #include "LanguageTranslator.h"
 
+#include "Logger.h"
+#include "StringUtils.h"
+
+LanguageTranslator::LanguageTranslator(std::wstring langFileName) {
+    CLOG(L"Loading skin XML: %s", langFileName.c_str());
+    std::string u8FileName = StringUtils::Narrow(langFileName);
+    tinyxml2::XMLError result = _xml.LoadFile(u8FileName.c_str());
+    if (result != tinyxml2::XMLError::XML_SUCCESS) {
+        if (result == tinyxml2::XMLError::XML_ERROR_FILE_NOT_FOUND) {
+            //Error::ErrorMessageDie(SKINERR_INVALID_SKIN);
+        }
+        throw std::logic_error("Failed to read XML file!");
+    }
+
+    _root = _xml.GetDocument()->FirstChildElement("translation");
+    if (_root == NULL) {
+        throw std::runtime_error("Could not find root XML element");
+    }
+}
+
 std::wstring LanguageTranslator::Translate(std::wstring str) {
     return str;
 }
--- 3RVX/LanguageTranslator.h
+++ 3RVX/LanguageTranslator.h
@@ -2,8 +2,15 @@
 
 #include <string>
 
-class LanguageTranslator {
+#include "TinyXml2\tinyxml2.h"
 
+class LanguageTranslator {
 public:
+    LanguageTranslator(std::wstring langFileName);
+
     std::wstring Translate(std::wstring str);
+
+private:
+    tinyxml2::XMLDocument _xml;
+    tinyxml2::XMLElement *_root;
 };

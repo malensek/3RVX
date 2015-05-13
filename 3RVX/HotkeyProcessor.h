cc5edd2b243b41f7160602f984aa81674d34befe
Matthew Malensek <matt@malensek.net>
2015-05-12 23:51:10 -0600

    Add summary for the HotkeyProcessor interface

--- 3RVX/HotkeyProcessor.h
+++ 3RVX/HotkeyProcessor.h
@@ -2,6 +2,9 @@
 
 class HotkeyInfo;
 
+/// <summary>
+/// Interface for classes that are able to handle and process hotkey events.
+/// </summary>
 class HotkeyProcessor {
     virtual void ProcessHotkeys(HotkeyInfo &hki) = 0;
 };

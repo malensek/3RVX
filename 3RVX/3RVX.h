c053fdfbe714d71210728b72f8b35a31cbe95ef6
Matthew Malensek <matt@malensek.net>
2015-05-12 23:47:40 -0600

    Remove unused definitions

--- 3RVX/3RVX.h
+++ 3RVX/3RVX.h
@@ -11,6 +11,3 @@ static const UINT WM_3RVX_SETTINGSCTRL
 #define MSG_SETTINGS WM_APP + 101
 #define MSG_EXIT     WM_APP + 102
 #define MSG_HIDEOSD  WM_APP + 103
-
-#define OSD_VOLUME 1
-#define OSD_MUTE 2

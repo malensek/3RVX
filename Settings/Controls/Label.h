aa470966fb84e16c92c210236cc5348a3e4c7bb7
Matthew Malensek <matt@malensek.net>
2015-05-16 02:44:52 -0600

    Test the new width functionality

--- Settings/Controls/Label.h
+++ Settings/Controls/Label.h
@@ -20,17 +20,19 @@ public:
             return false;
         }
 
+        Width(TextDimensions().cx);
 
-        /* Determine the width of the text */
-        HDC dc = GetDC(_hWnd);
-        SIZE sz = { 0 };
-        GetTextExtentPoint32(dc, &text[0], text.size(), &sz);
 
-        /* Update the window size based on the text size */
-        RECT r = Control::ScreenDimensions();
-        POINT pt = { r.left, r.top };
-        ScreenToClient(_parent, &pt);
-        MoveWindow(_hWnd, pt.x, pt.y, sz.cx, sz.cy, TRUE);
+//        /* Determine the width of the text */
+//        HDC dc = GetDC(_hWnd);
+//        SIZE sz = { 0 };
+//        GetTextExtentPoint32(dc, &text[0], text.size(), &sz);
+
+//        /* Update the window size based on the text size */
+//        RECT r = Control::ScreenDimensions();
+//        POINT pt = { r.left, r.top };
+//        ScreenToClient(_parent, &pt);
+//        MoveWindow(_hWnd, pt.x, pt.y, sz.cx, sz.cy, TRUE);
         return true;
     }
 };

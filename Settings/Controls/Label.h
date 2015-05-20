b10e6db798cac8c483d635bba27660a57065813c
Matthew Malensek <matt@malensek.net>
2015-05-20 15:59:04 -0600

    Disable label auto-sizing

--- Settings/Controls/Label.h
+++ Settings/Controls/Label.h
@@ -13,26 +13,4 @@ public:
     Control(id, parent) {
 
     }
-
-    virtual bool Text(std::wstring text) {
-        bool result = Control::Text(text);
-        if (result == false) {
-            return false;
-        }
-
-        Width(TextDimensions().cx);
-
-
-//        /* Determine the width of the text */
-//        HDC dc = GetDC(_hWnd);
-//        SIZE sz = { 0 };
-//        GetTextExtentPoint32(dc, &text[0], text.size(), &sz);
-
-//        /* Update the window size based on the text size */
-//        RECT r = Control::ScreenDimensions();
-//        POINT pt = { r.left, r.top };
-//        ScreenToClient(_parent, &pt);
-//        MoveWindow(_hWnd, pt.x, pt.y, sz.cx, sz.cy, TRUE);
-        return true;
-    }
 };

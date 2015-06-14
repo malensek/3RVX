1d9974d7370e2184c60a8125c85cd5a897830a9d
Matthew Malensek <matt@malensek.net>
2015-06-13 23:28:35 -0600

    delete auto-generated components file

--- Installer/BuildInstaller.bat
+++ Installer/BuildInstaller.bat
@@ -2,4 +2,4 @@ set PATH=%PATH%;%WIX%\bin
 heat dir ../Release -gg -srd -cg 3RVXComponents -dr INSTALLFOLDER -out 3RVXComponents.wxs
 candle ./3RVX.wxs ./3RVXComponents.wxs
 light -b ../Release -ext WixUIExtension ./3RVX.wixobj ./3RVXComponents.wixobj -o 3RVX.msi
-del 3RVX.wixobj 3RVX.wixpdb 3RVXComponents.wixobj cab1.cab
+del 3RVX.wixobj 3RVX.wixpdb 3RVXComponents.wxs 3RVXComponents.wixobj cab1.cab

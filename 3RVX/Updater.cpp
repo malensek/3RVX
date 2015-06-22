1a58f52fa0522d217e316bb5a12135c1ad6cc499
Matthew Malensek <matt@malensek.net>
2015-06-22 00:57:27 -0600

    Finish remote version checking functionality

--- 3RVX/Updater.cpp
+++ 3RVX/Updater.cpp
@@ -3,12 +3,20 @@
 #pragma comment(lib, "wininet.lib")
 #include <Windows.h>
 #include <WinInet.h>
+#include <sstream>
 
+#include "StringUtils.h"
 #include "Logger.h"
 
 bool Updater::NewerVersionAvailable() {
+    std::pair<int, int> version = RemoteVersion();
+    CLOG(L"Remote version: %d.%d", version.first, version.second);
+    return false;
+}
+
+std::pair<int, int> Updater::RemoteVersion() {
     HINTERNET internet = InternetOpen(
-        L"3RVX-Updater",
+        L"3RVX Updater",
         INTERNET_OPEN_TYPE_PRECONFIG,
         NULL,
         NULL,
@@ -16,7 +24,7 @@ bool Updater::NewerVersionAvailable() {
 
     HINTERNET connection = InternetOpenUrl(
         internet,
-        L"http://matthew.malensek.net",
+        L"http://matthew.malensek.net/projects/3RVX/latest_version",
         NULL,
         0,
         INTERNET_FLAG_NO_CACHE_WRITE | INTERNET_FLAG_PRAGMA_NOCACHE,
@@ -24,10 +32,30 @@ bool Updater::NewerVersionAvailable() {
 
     if (connection == NULL) {
         CLOG(L"Could not connect to URL!");
-        return false;
+        return std::pair<int, int>(0, 0);
     }
 
-    char buf[16];
+    std::string str("");
+    char buf[32];
     DWORD read;
-    InternetReadFile(connection, buf, 16, &read);
+    while (InternetReadFile(connection, buf, 16, &read) == TRUE && read != 0) {
+        str.append(buf);
+    }
+
+    /* Only consider the first line */
+    str.erase(str.find('\n'), str.size() - 1);
+
+    int dot = str.find('.');
+    std::string major = str.substr(0, dot);
+    std::string minor = str.substr(dot + 1, str.size());
+
+    std::pair<int, int> version;
+    std::istringstream ss;
+
+    ss = std::istringstream(major);
+    ss >> version.first;
+    ss = std::istringstream(minor);
+    ss >> version.second;
+
+    return version;
 }
--- 3RVX/Updater.h
+++ 3RVX/Updater.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <utility>
+
 class Updater {
 public:
     static bool NewerVersionAvailable();
+
+private:
+    static std::pair<int, int> RemoteVersion();
 
 };

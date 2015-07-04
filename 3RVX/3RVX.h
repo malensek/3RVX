da5a68aff87e0152a5058c070b634eecaddf1a27
Matthew Malensek <matt@malensek.net>
2015-07-03 23:59:45 -0600

    Add a class for the settings dialog

--- 3RVX/3RVX.h
+++ 3RVX/3RVX.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define CLASS_3RVX L"3RVXv3"
+#define CLASS_3RVX_SETTINGS L"3RVX-Settings"
 
 static const UINT WM_3RVX_CONTROL
     = RegisterWindowMessage(L"WM_3RVX_CONTROL");
@@ -11,3 +12,4 @@ static const UINT WM_3RVX_SETTINGSCTRL
 #define MSG_SETTINGS WM_APP + 101
 #define MSG_EXIT     WM_APP + 102
 #define MSG_HIDEOSD  WM_APP + 103
+#define MSG_ACTIVATE WM_APP + 104

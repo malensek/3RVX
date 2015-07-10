9d9afc8024f34487ea457e46ab938363a2cafa8a
Matthew Malensek <matt@malensek.net>
2015-07-09 21:34:06 -0600

    Add version number to the MSI installer

--- Installer/BuildInstaller.bat
+++ Installer/BuildInstaller.bat
@@ -20,7 +20,7 @@ ECHO ON
 set PATH=%PATH%;%WIX%\bin
 heat dir ../Release -gg -srd -cg 3RVXComponents -dr INSTALLFOLDER -out 3RVXComponents.wxs
 candle -pedantic -dProductVersion=%version%.0.0 ./3RVX.wxs ./3RVXComponents.wxs
-light -b ../Release -ext WixUIExtension ./3RVX.wixobj ./3RVXComponents.wixobj -o 3RVX.msi
+light -b ../Release -ext WixUIExtension ./3RVX.wixobj ./3RVXComponents.wixobj -o 3RVX-%version%.msi
 del 3RVX.wixobj 3RVX.wixpdb 3RVXComponents.wxs 3RVXComponents.wixobj
 
 @ECHO OFF

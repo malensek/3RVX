f9ecebb6b6a8be2071e479bdec7c2fef8d54a93c
Matthew Malensek <matt@malensek.net>
2015-07-25 01:53:43 -0600

    Prepare for separate skin format definitions

---
+++ 3RVX/Skin/SkinV3.cpp
@@ -0,0 +1,1 @@
+#include "SkinV3.h"
--- 3RVX/Skin/SkinV3.h
+++ 3RVX/Skin/SkinV3.h
@@ -1,1 +1,29 @@
 #pragma once
+
+#pragma comment(lib, "gdiplus.lib")
+
+#include <Windows.h>
+#include <gdiplus.h>
+
+#include <string>
+#include <vector>
+
+#include "../TinyXml2/tinyxml2.h"
+#include "Skin.h"
+
+class Meter;
+class OSDSkin;
+class SliderKnob;
+class SoundPlayer;
+
+class SkinV3 : public Skin {
+public:
+    SkinV3(std::wstring skinXML);
+    ~SkinV3();
+
+private:
+    OSDComponent *_volumeOSD;
+    OSDComponent *_muteOSD;
+    OSDComponent *_ejectOSD;
+
+};

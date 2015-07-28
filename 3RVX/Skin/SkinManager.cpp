b3d38ba7485f6a0fc4b9548f627fdd63ff6d5a6a
Matthew Malensek <matt@malensek.net>
2015-07-27 18:01:47 -0600

    Update SkinManager to manage skin state/memory

--- 3RVX/Skin/SkinManager.cpp
+++ 3RVX/Skin/SkinManager.cpp
@@ -3,6 +3,10 @@
 #include "Skin.h"
 #include "SkinV3.h"
 
+#include "MeterComponent.h"
+#include "OSDComponent.h"
+#include "SliderComponent.h"
+
 SkinManager *SkinManager::instance;
 
 SkinManager *SkinManager::Instance() {
@@ -12,15 +16,54 @@ SkinManager *SkinManager::Instance() {
     return instance;
 }
 
-SkinManager::~SkinManager() {
-    delete _skin;
-}
-
 void SkinManager::LoadSkin(std::wstring skinXML) {
-    delete _skin;
-    _skin = new SkinV3(skinXML);
+    DisposeComponents();
+
+    Skin *skin = new SkinV3(skinXML);
+
+    _volumeOSD = skin->VolumeOSD();
+    _volumeIconset = skin->VolumeIconset();
+    _volumeSlider = skin->VolumeSlider();
+    _muteOSD = skin->MuteOSD();
+    _ejectOSD = skin->EjectOSD();
 }
 
-Skin *SkinManager::CurrentSkin() {
-    return _skin;
+OSDComponent *SkinManager::VolumeOSD() {
+    return _volumeOSD;
+}
+
+std::vector<HICON> &SkinManager::VolumeIconset() {
+    return _volumeIconset;
+}
+
+SliderComponent *SkinManager::VolumeSlider() {
+    return _volumeSlider;
+}
+
+OSDComponent *SkinManager::MuteOSD() {
+    return _muteOSD;
+}
+
+OSDComponent *SkinManager::EjectOSD() {
+    return _ejectOSD;
+}
+
+SkinManager::~SkinManager() {
+    DisposeComponents();
+}
+
+void SkinManager::DisposeComponents() {
+    delete _volumeOSD;
+    _volumeOSD = NULL;
+    for (HICON icon : _volumeIconset) {
+        DestroyIcon(icon);
+    }
+    _volumeIconset.clear();
+    delete _volumeSlider;
+
+    delete _muteOSD;
+    _muteOSD = NULL;
+
+    delete _ejectOSD;
+    _ejectOSD = NULL;
 }
--- 3RVX/Skin/SkinManager.h
+++ 3RVX/Skin/SkinManager.h
@@ -1,18 +1,39 @@
 #pragma once
 
+#include <Windows.h>
+
 #include <string>
+#include <vector>
 
 class Skin;
+struct OSDComponent;
+struct SliderComponent;
 
 class SkinManager {
 public:
     static SkinManager *Instance();
     void LoadSkin(std::wstring skinXML);
-    Skin *CurrentSkin();
+
+    OSDComponent *VolumeOSD();
+    std::vector<HICON> &VolumeIconset();
+    SliderComponent *VolumeSlider();
+
+    OSDComponent *MuteOSD();
+
+    OSDComponent *EjectOSD();
+
+private:
+    OSDComponent *_volumeOSD;
+    std::vector<HICON> _volumeIconset;
+    SliderComponent *_volumeSlider;
+    OSDComponent *_muteOSD;
+
+    OSDComponent *_ejectOSD;
+
+    ~SkinManager();
+    void DisposeComponents();
 
 private:
     static SkinManager *instance;
-    Skin *_skin;
 
-    ~SkinManager();
 };

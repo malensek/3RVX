70f092e45f9e2b0e637a458870c4932f13b60f68
Matthew Malensek <matt@malensek.net>
2015-07-29 00:08:25 -0600

    Add method stubs

--- 3RVX/Skin/MultiSkin.h
+++ 3RVX/Skin/MultiSkin.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <Windows.h>
+
 #include "Skin.h"
 
 class MultiSkin : public Skin {
+public:
+    virtual OSDComponent *VolumeOSD();
+    virtual OSDComponent *MuteOSD();
+    virtual OSDComponent *EjectOSD();
+
+    virtual std::vector<HICON> VolumeIconset();
+
+    virtual SliderComponent *VolumeSlider();
 
 };

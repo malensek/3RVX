4d88cb17fa1f0351272abab98557c0314ebdac12
Matthew Malensek <matt@malensek.net>
2015-08-24 23:54:09 -0600

    Test update icon ballooon

--- Settings/UpdaterWindow.cpp
+++ Settings/UpdaterWindow.cpp
@@ -22,7 +22,10 @@ LRESULT UpdaterWindow::WndProc(
         switch (wParam) {
         case _3RVX::MSG_UPDATEICON:
             CLOG(L"Creating update icon");
-            new NotifyIcon(Window::Handle(), L"Update Available", _icon);
+            NotifyIcon *x = new NotifyIcon(Window::Handle(), L"Update Available", _icon);
+            HICON ic;
+            LoadIconMetric(Window::InstanceHandle(), MAKEINTRESOURCE(IDI_MAINICON), LIM_LARGE, &ic);
+            x->Balloon(L"Update Available", L"3RVX 3.2", ic);
             break;
         }
 

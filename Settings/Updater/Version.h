64635220db85ed16109ee33387884bf5aa42bcaf
Matthew Malensek <matt@malensek.net>
2015-08-26 19:21:12 -0600

    Update instance variables, add getters

--- Settings/Updater/Version.h
+++ Settings/Updater/Version.h
@@ -4,22 +4,29 @@
 
 struct Version {
 public:
-    const int major;
-    const int minor;
-    const int revision;
+    Version(int major = 0, int minor = 0, int revision = 0) :
+    _major(major),
+    _minor(minor),
+    _revision(revision) {
 
-    Version(int major, int minor = 0, int revision = 0) :
-    major(major),
-    minor(minor),
-    revision(revision) {
+    }
+
+    const int Major() { return _major; }
+    const int Minor() { return _minor; }
+    const int Revision() { return _revision; }
 
     }
 
     std::wstring ToString() {
-        return std::to_wstring(major)
+        return std::to_wstring(_major)
             + L"."
-            + std::to_wstring(minor)
+            + std::to_wstring(_minor)
             + L"."
-            + std::to_wstring(revision);
+            + std::to_wstring(_revision);
     }
+
+private:
+    int _major;
+    int _minor;
+    int _revision;
 };

ee9e6b7313651af9bdc20040d2cdebda4cc9ffbf
Matthew Malensek <matt@malensek.net>
2015-08-31 23:57:39 -0600

    Add documentation

--- Settings/UITranslator.h
+++ Settings/UITranslator.h
@@ -3,9 +3,19 @@
 #include <Windows.h>
 #include <string>
 
+/// <summary>
+/// Handles translation of UI elements defined as Win32 controls.
+/// </summary>
 class UITranslator {
 public:
+    /// <summary>
+    /// Reads a control's text, translates it, and updates the text.
+    /// </summary>
     static void TranslateControlText(HWND hWnd, int dlgItem);
+
+    /// <summary>
+    /// Sets a control's text to the translation of the provided string.
+    /// </summary>
     static void TranslateControlText(HWND hWnd, int dlgItem, std::wstring &str);
     static void TranslateWindowText(HWND hWnd);
 };

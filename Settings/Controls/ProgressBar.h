c8a66e76734dc2f6066b113aba3085c20f34f00b
Matthew Malensek <matt@malensek.net>
2015-09-06 23:49:23 -0600

    Add basic implementation of progress bar methods

--- Settings/Controls/ProgressBar.h
+++ Settings/Controls/ProgressBar.h
@@ -2,12 +2,25 @@
 
 #include "Control.h"
 #include "StatusCallback.h"
+#include <CommCtrl.h>
 
 class ProgressBar : public Control, public StatusCallback {
 public:
     ProgressBar(int id, Dialog &parent, bool translate = true) :
     Control(id, parent, translate) {
 
+    }
+
+    void Range(int min, int max) {
+        SendMessage(_hWnd, PBM_SETRANGE32, min, max);
+    }
+
+    void Position(int pos) {
+        SendMessage(_hWnd, PBM_SETPOS, pos, 0);
+    }
+
+    void Marquee(bool enabled, int refresh = 30) {
+        SendMessage(_hWnd, PBM_SETMARQUEE, enabled, refresh);
     }
 
 protected:

497c2cbbfa9aacc755028a3fca34da773a11baff
Matthew Malensek <matt@malensek.net>
2015-09-09 23:59:44 -0600

    Add license info

--- Settings/Controls/Button.cpp
+++ Settings/Controls/Button.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2015, Matthew Malensek.
+// Distributed under the BSD 2-Clause License (see LICENSE.txt for details)
+
 #include "Button.h"
 
 BOOL CALLBACK Button::Command(unsigned short nCode) {

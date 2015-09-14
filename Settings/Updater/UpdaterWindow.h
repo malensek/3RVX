672a86b3641d17da18a09882b65d7623e0e80f57
Matthew Malensek <matt@malensek.net>
2015-09-13 22:28:34 -0600

    Add language strings

--- Settings/Updater/UpdaterWindow.h
+++ Settings/Updater/UpdaterWindow.h
@@ -34,6 +34,12 @@ private:
         WPARAM wParam, LPARAM lParam);
 
 private:
+    std::wstring _availableStr = L"Update Available";
+    std::wstring _updateVersStr = L"3RVX {1}";
+    std::wstring _menuInstallStr = L"Install";
+    std::wstring _menuIgnoreStr = L"Ignore version";
+    std::wstring _menuRemindStr = L"Remind me later";
+
     static const int MENU_INSTALL = 0;
     static const int MENU_IGNORE = 1;
     static const int MENU_REMIND = 2;

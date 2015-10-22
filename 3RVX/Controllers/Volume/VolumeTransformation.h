090cf42f736ad66ecc9360cf2770d83f8ea30574
Matthew Malensek <matt@malensek.net>
2015-10-22 13:09:03 -0600

    Add methods to apply multiple transformations

--- 3RVX/Controllers/Volume/VolumeTransformation.h
+++ 3RVX/Controllers/Volume/VolumeTransformation.h
@@ -14,4 +14,31 @@ public:
     /// by the ToVirtual() function).
     /// </summary>
     virtual float Revert(float vol) = 0;
+
+    /// <summary>
+    /// Applies several transformations supplied as a vector to the initial
+    /// value provided. The transformations are applied in order of appearance 
+    /// in the vector.
+    /// </summary>
+    static float ApplyTransformations(
+            std::vector<VolumeTransformation *> &transforms, float initialVal) {
+        float newVal = initialVal;
+        for (VolumeTransformation *trans : transforms) {
+            newVal = trans->Apply(newVal);
+        }
+        return newVal;
+    }
+
+    /// <summary>
+    /// Reverts several transformations, starting with a transformed ("virtual")
+    /// value. This method reverts the transformations in reverse order.
+    /// </summary>
+    static float RevertTransformations(
+            std::vector<VolumeTransformation *> &transforms, float virtVal) {
+        float newVal = virtVal;
+        for (auto it = transforms.rbegin(); it != transforms.rend(); ++it) {
+            newVal = (*it)->Revert(newVal);
+        }
+        return newVal;
+    }
 };

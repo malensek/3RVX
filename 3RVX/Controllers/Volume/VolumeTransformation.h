72d08adcc89f72f2243c063b4ec3f7202574807f
Matthew Malensek <matt@malensek.net>
2015-10-22 10:20:51 -0600

    Split transform methods into to/from

--- 3RVX/Controllers/Volume/VolumeTransformation.h
+++ 3RVX/Controllers/Volume/VolumeTransformation.h
@@ -2,5 +2,16 @@
 
 class VolumeTransformation {
 public:
-    virtual float Transform(float vol) = 0;
+    /// <summary>
+    /// Transforms a given volume level to a new ("virtual") level based on a
+    /// formula or set of rules (e.g., a volume curve transformation).
+    /// </summary>
+    virtual float ToVirtual(float vol) = 0;
+
+    /// <summary>
+    /// Given a transformed ("virtual") volume value, this function reverts it 
+    /// back to its original value (assuming the given value was produced
+    /// by the ToVirtual() function).
+    /// </summary>
+    virtual float FromVirtual(float vol) = 0;
 };

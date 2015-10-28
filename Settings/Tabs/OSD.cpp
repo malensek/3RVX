1b0c2bf98c62b75a933ce7dc2c347a11b4a45f37
Matthew Malensek <matt@malensek.net>
2015-10-28 11:53:52 -0600

    Add OSDs to the lsit

--- Settings/Tabs/OSD.cpp
+++ Settings/Tabs/OSD.cpp
@@ -8,8 +8,11 @@ SettingsTab(hInstance, tabTemplate, title) {
 }
 
 void OSD::Initialize() {
-
-
+    ListBox *lb = new ListBox(LST_OSDS, *this);
+    lb->AddItem(L"Volume");
+    lb->AddItem(L"Brightness");
+    lb->AddItem(L"Eject");
+    lb->AddItem(L"Keyboard");
 }
 
 void OSD::LoadSettings() {

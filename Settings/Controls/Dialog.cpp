fe2873f8b8d5079b02607d7c0d36d0e50ee44e58
Matthew Malensek <matt@malensek.net>
2015-10-27 18:40:33 -0600

    Incorporate DialogBase into the Dialog class

--- Settings/Controls/Dialog.cpp
+++ Settings/Controls/Dialog.cpp
@@ -33,23 +33,6 @@ HWND Dialog::DialogHandle() {
     return _dlgHwnd;
 }
 
-INT_PTR Dialog::StaticDialogProc(HWND hwndDlg, UINT uMsg,
-        WPARAM wParam, LPARAM lParam) {
-    Dialog *dlg;
-
-    if (uMsg == WM_INITDIALOG) {
-        dlg = (Dialog *) lParam;
-        SetWindowLongPtr(hwndDlg, DWLP_USER, (LONG_PTR) dlg);
-    } else {
-        dlg = (Dialog *) GetWindowLongPtr(hwndDlg, DWLP_USER);
-        if (!dlg) {
-            return FALSE;
-        }
-    }
-
-    return dlg->DialogProc(hwndDlg, uMsg, wParam, lParam);
-}
-
 INT_PTR Dialog::DialogProc(HWND hwndDlg, UINT uMsg,
         WPARAM wParam, LPARAM lParam) {
 
@@ -77,7 +60,6 @@ INT_PTR Dialog::DialogProc(HWND hwndDlg, UINT uMsg,
             return FALSE;
         }
     }
-
 
     return FALSE;
 }
--- Settings/Controls/Dialog.h
+++ Settings/Controls/Dialog.h
@@ -6,10 +6,11 @@
 #include <unordered_map>
 
 #include "../../3RVX/Window.h"
+#include "DialogBase.h"
 
 class Control;
 
-class Dialog : public Window {
+class Dialog : DialogBase, public Window {
 public:
     Dialog(LPCWSTR className, LPCWSTR dlgTemplate);
 
@@ -18,15 +19,11 @@ public:
     HWND DialogHandle();
 
 protected:
-    static INT_PTR CALLBACK StaticDialogProc(HWND hwndDlg, UINT uMsg,
-        WPARAM wParam, LPARAM lParam);
-
-    virtual INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg,
-        WPARAM wParam, LPARAM lParam);
-
     HWND _dlgHwnd;
 
     /// <summary>Maps control IDs to their respective instances.</summary>
     std::unordered_map<int, Control *> _controlMap;
 
+    virtual INT_PTR CALLBACK DialogProc(HWND hwndDlg, UINT uMsg,
+        WPARAM wParam, LPARAM lParam);
 };

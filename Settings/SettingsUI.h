39d49e2cdb5d373216f6929ef43d9a7661640ee4
Matthew Malensek <matt@malensek.net>
2015-11-03 23:56:16 -0700

    Add header license stubs

--- Settings/SettingsUI.h
+++ Settings/SettingsUI.h
@@ -1,3 +1,6 @@
+// Copyright (c) 2015, Matthew Malensek.
+// Distributed under the BSD 2-Clause License (see LICENSE.txt for details)
+
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
--- Settings/targetver.h
+++ Settings/targetver.h
@@ -1,3 +1,6 @@
+// Copyright (c) 2015, Matthew Malensek.
+// Distributed under the BSD 2-Clause License (see LICENSE.txt for details)
+
 #pragma once
 
 // Including SDKDDKVer.h defines the highest available Windows platform.

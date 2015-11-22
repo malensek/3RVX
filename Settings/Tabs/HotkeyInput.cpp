bc838f21bea3d109e5a5c23065592ffc24a3e49f
Matthew Malensek <matt@malensek.net>
2015-11-21 22:28:39 -0700

    Start refactoring hotkey input prompt

---
+++ Settings/Tabs/HotkeyInput.cpp
@@ -0,0 +1,30 @@
+#include "HotkeyInput.h"
+
+#include "KeyGrabber.h"
+
+INT_PTR HotkeyInput::DialogProc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_INITDIALOG:
+        KeyGrabber::Instance()->SetHwnd(hwndDlg);
+        KeyGrabber::Instance()->Grab();
+        break;
+
+    case WM_CLOSE:
+//        CLOG(L"Closing hotkey prompt.");
+//        EnableWindow(_parent, TRUE);
+//        EndDialog(hwndDlg, 0);
+//        DestroyWindow(_hWnd);
+        break;
+
+    case WM_COMMAND:
+//        nCode = HIWORD(wParam);
+//        ctrlId = LOWORD(wParam);
+//        if (ctrlId == BTN_CANCEL && nCode == BN_CLICKED) {
+//            SendMessage(hwndDlg, WM_CLOSE, NULL, NULL);
+//        }
+        break;
+    }
+
+
+    return INT_PTR();
+}
---
+++ Settings/Tabs/HotkeyInput.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2015, Matthew Malensek.
+// Distributed under the BSD 2-Clause License (see LICENSE.txt for details)
+
+#pragma once
+
+#include "../Controls/Dialog.h"
+
+class HotkeyInput : public Dialog {
+public:
+
+protected:
+    virtual INT_PTR CALLBACK DialogProc(
+        HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
+};

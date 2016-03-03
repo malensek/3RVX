11033d2b058ca9b2c4a659e5ea331bf185d0d538
Matthew Malensek <matt@malensek.net>
2016-03-03 10:58:55 -0700

    Add documentation

--- 3RVX/DriveInfo.h
+++ 3RVX/DriveInfo.h
@@ -8,7 +8,16 @@ class DriveInfo {
 public:
     DriveInfo(wchar_t driveLetter);
 
+    /// <summary>
+    /// Given a drive letter, this function retrieves the drive file name
+    /// (something of the form \\.\X:), useful for IOCTL operations.
+    /// </summary>
     static std::wstring DriveFileName(wchar_t &driveLetter);
+
+    /// <summary>
+    /// Given a drive letter, this function retrieves the drive file name
+    /// (something of the form \\.\X:), useful for IOCTL operations.
+    /// </summary>
     static std::wstring DriveFileName(std::wstring &driveLetter);
 
 private:
@@ -19,8 +28,20 @@ private:
     bool _hasRemovableMedia;
     bool _isHotplug;
 
+    /// <summary>Retrieves the numeric identifier for this drive.</summary>
     void PopulateDeviceId();
+
+    /// <summary>
+    /// Retrieves information about the specific device: product name,
+    /// manufacturer, etc.
+    /// </summary>
     void PopulateDeviceInfo();
+
+
+    /// <summary>
+    /// Determines hotplug characteristics for a drive. This includes
+    /// whether the drive has removable media.
+    /// </summary>
     void PopulateHotplugInfo();
 
 };

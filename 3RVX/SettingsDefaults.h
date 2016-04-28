a827c1facd4eba0f308796e9cda7a8815c06c7f8
Matthew Malensek <matt@malensek.net>
2016-04-28 07:14:29 -0600

    Add string constants and reorganize defaults

--- 3RVX/SettingsDefaults.h
+++ 3RVX/SettingsDefaults.h
@@ -5,12 +5,27 @@
 
 class SettingsDefaults {
 public:
-    /* Default settings */
+    /* String Constants*/
+    static constexpr const wchar_t *Language = L"English";
+    static constexpr const wchar_t *Skin = L"Classic";
+    static constexpr const wchar_t *MainAppName = L"3RVX.exe";
+    static constexpr const wchar_t *SettingsAppName = L"Settings.exe";
+    static constexpr const wchar_t *SettingsFileName = L"Settings.xml";
+    static constexpr const wchar_t *LanguageDirName = L"Languages";
+    static constexpr const wchar_t *SkinDirName = L"Skins";
+    static constexpr const wchar_t *SkinFileName = L"Skin.xml";
+
+    /* OSDs */
+    static const bool VolumeOSDEnabled = true;
+    static const bool EjectOSDEnabled = true;
+    static const bool BrightnessOSDEnabled = true;
+    static const bool KeyboardOSDEnabled = false;
+
     static const bool OnTop = true;
-    static const AnimationTypes::HideAnimation DefaultHideAnim
-        = AnimationTypes::Fade;
     static const bool HideFullscreen = false;
     static const bool HideDirectX = false;
+    static const AnimationTypes::HideAnimation DefaultHideAnim
+        = AnimationTypes::Fade;
     static const int HideSpeed = 765;
     static const int HideTime = 800;
     static constexpr const float VolumeLimit = 1.0f;
@@ -24,8 +39,4 @@ public:
     static const bool SubscribeVolumeEvents = true;
     static const bool SubscribeEjectEvents = true;
 
-    static const bool VolumeOSDEnabled = true;
-    static const bool EjectOSDEnabled = true;
-    static const bool BrightnessOSDEnabled = true;
-    static const bool KeyboardOSDEnabled = false;
 };

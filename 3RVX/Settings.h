be728217fe9fcc3f440580c1e425711e2ea968c4
Matthew Malensek <matt@malensek.net>
2016-04-28 07:15:17 -0600

    Remove unused constants

--- 3RVX/Settings.h
+++ 3RVX/Settings.h
@@ -190,7 +190,7 @@ private:
 
     tinyxml2::XMLElement *GetOrCreateElement(std::string elementName);
 
-public:
+private:
     static constexpr const wchar_t *MAIN_APP = L"3RVX.exe";
     static constexpr const wchar_t *SETTINGS_APP = L"Settings.exe";
     static constexpr const wchar_t *SETTINGS_FILE = L"Settings.xml";
@@ -198,9 +198,7 @@ public:
     static constexpr const wchar_t *SKIN_DIR = L"Skins";
     static constexpr const wchar_t *SKIN_XML = L"Skin.xml";
 
-    static constexpr const wchar_t *DefaultLanguage = L"English";
-    static constexpr const wchar_t *DefaultSkin = L"Classic";
-
+private:
     /* XML tag names */
     static constexpr const char *XML_AUDIODEV = "audioDeviceID";
     static constexpr const char *XML_CURVE_ADJUST = "curveAdjust";

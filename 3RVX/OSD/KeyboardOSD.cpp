711ac12b604cf315f3e624889d338a83bec5f1e1
Matthew Malensek <matt@malensek.net>
2016-06-11 22:46:20 -0600

    Constructor stub

--- 3RVX/OSD/KeyboardOSD.cpp
+++ 3RVX/OSD/KeyboardOSD.cpp
@@ -2,3 +2,9 @@
 // Distributed under the BSD 2-Clause License (see LICENSE.txt for details)
 
 #include "KeyboardOSD.h"
+
+KeyboardOSD::KeyboardOSD() :
+OSD(L"3RVX-KeyOSDDispatcher"),
+_mWnd(L"3RVX-KeyboardOSD", L"3RVX-KeyboardOSD") {
+
+}

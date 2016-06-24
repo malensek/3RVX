142f635096a3c76946da848a6cc641152e118b40
Matthew Malensek <matt@malensek.net>
2016-06-23 23:56:30 -0600

    Add class for retrieving system color scheme settings

---
+++ 3RVX/Skin/SystemColors.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <Windows.h>
+#include "../Logger.h"
+#include <math.h>
+#include <algorithm>
+
+#include <dwmapi.h>
+#pragma comment(lib, "Dwmapi.lib")
+
+class SystemColors {
+
+    struct DWMColorizationParams {
+        UINT ColorizationColor;
+        UINT ColorizationAfterglow;
+        UINT ColorizationColorBalance;
+        UINT ColorizationAfterglowBalance;
+        UINT ColorizationBlurBalance;
+        UINT ColorizationGlassReflectionIntensity;
+        UINT ColorizationOpaqueBlend;
+    };
+
+public:
+    SystemColors() {
+        HMODULE dwm = LoadLibrary(L"dwmapi.dll");
+        HRESULT(WINAPI *DwmGetColorizationParameters) (DWMColorizationParams *colorparam);
+        *(FARPROC *) &DwmGetColorizationParameters = GetProcAddress(dwm, (LPCSTR) 127);
+        DWMColorizationParams params;
+        DwmGetColorizationParameters(&params);
+
+        CLOG(L"-> %x", params.ColorizationColor);
+        CLOG(L"-> %x", params.ColorizationAfterglow);
+        CLOG(L"-> %d", params.ColorizationColorBalance);
+        CLOG(L"-> %d", params.ColorizationAfterglowBalance);
+        CLOG(L"-> %x", params.ColorizationBlurBalance);
+        CLOG(L"-> %x", params.ColorizationGlassReflectionIntensity);
+        CLOG(L"-> %x", params.ColorizationOpaqueBlend);
+
+        COLORREF base = RGB(217, 217, 217);
+
+        DWORD val;
+        BOOL opaque;
+        DwmGetColorizationColor(&val, &opaque);
+
+        CLOG(L"=> %x", val);
+
+    }
+
+
+};

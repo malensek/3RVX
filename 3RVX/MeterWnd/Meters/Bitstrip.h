fe852169ad91bfe046380d99c659224809a19ed4
Matthew Malensek <matt@malensek.net>
2016-06-26 17:44:10 -0600

    Add documentation comments

--- 3RVX/MeterWnd/Meters/Bitstrip.h
+++ 3RVX/MeterWnd/Meters/Bitstrip.h
@@ -5,6 +5,18 @@
 
 #include "../Meter.h"
 
+/**
+ * A bitstrip meter allows for full customizability of each meter state and
+ * resembles a strip of film; each successive state image is placed after the
+ * previous and then divided into discrete image "tiles." This allows full
+ * control of the display for cases where the other meter types are not flexible
+ * enough. For example, a bitstrip could be used to make a slideshow where each
+ * change in volume, brightness, etc. shows a completely different image.
+ *
+ * For an live sample, see the "Ignition" skin.
+ *
+ * @author malensek
+ */
 class Bitstrip : public Meter {
 public:
     Bitstrip(std::wstring bitmapName, int x, int y, int units);

8f4b58ef6ad9c36ca88ae1e146202da2d0cfbad8
Matthew Malensek <matt@malensek.net>
2016-06-30 18:05:32 -0600

    Move functionality to implementation file

---
+++ 3RVX/Skin/SystemColors.cpp
@@ -0,0 +1,42 @@
+#include "SystemColors.h"
+
+#include <dwmapi.h>
+
+#include "../Logger.h"
+
+SystemColors::SystemColors() {
+    HMODULE dwm = LoadLibrary(L"dwmapi.dll");
+    if (dwm == NULL) {
+        _useUndocumented = false;
+    }
+
+    *(FARPROC *) &DwmGetColorizationParameters
+        = GetProcAddress(dwm, (LPCSTR) ColorizationParamOrd);
+    if (DwmGetColorizationParameters == nullptr) {
+        _useUndocumented = false;
+    }
+
+    DwmColorizationParams params = { 0 };
+    HRESULT hr = DwmGetColorizationParameters(&params);
+    if (FAILED(hr)) {
+        CLOG(L"couldn't get dwm params");
+    }
+
+    CLOG(L"-> %x", params.ColorizationColor);
+    CLOG(L"-> %x", params.ColorizationAfterglow);
+    CLOG(L"-> %d", params.ColorizationColorBalance);
+    CLOG(L"-> %d", params.ColorizationAfterglowBalance);
+    CLOG(L"-> %x", params.ColorizationBlurBalance);
+    CLOG(L"-> %x", params.ColorizationGlassReflectionIntensity);
+    CLOG(L"-> %x", params.ColorizationOpaqueBlend);
+
+    DWORD val;
+    BOOL opaque;
+    DwmGetColorizationColor(&val, &opaque);
+
+    CLOG(L"=> %x", val);
+}
+
+UINT SystemColors::ColorizationColor() const {
+    return 0;
+}
--- 3RVX/Skin/SystemColors.h
+++ 3RVX/Skin/SystemColors.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <Windows.h>
-#include "../Logger.h"
-#include <math.h>
-#include <algorithm>
-
-#include <dwmapi.h>
 
 class SystemColors {
 
+public:
+    SystemColors();
+
+    UINT ColorizationColor() const;
+
+private:
     struct DwmColorizationParams {
         UINT ColorizationColor;
         UINT ColorizationAfterglow;
@@ -19,34 +20,11 @@ class SystemColors {
         UINT ColorizationOpaqueBlend;
     };
 
-public:
-    SystemColors() {
-        HMODULE dwm = LoadLibrary(L"dwmapi.dll");
-        *(FARPROC *) &DwmGetColorizationParameters = GetProcAddress(dwm, (LPCSTR) 127);
-        DwmColorizationParams params = { 0 };
-        HRESULT hr = DwmGetColorizationParameters(&params);
-        if (FAILED(hr)) {
-            CLOG(L"couldn't get dwm params");
-        }
-
-        CLOG(L"-> %x", params.ColorizationColor);
-        CLOG(L"-> %x", params.ColorizationAfterglow);
-        CLOG(L"-> %d", params.ColorizationColorBalance);
-        CLOG(L"-> %d", params.ColorizationAfterglowBalance);
-        CLOG(L"-> %x", params.ColorizationBlurBalance);
-        CLOG(L"-> %x", params.ColorizationGlassReflectionIntensity);
-        CLOG(L"-> %x", params.ColorizationOpaqueBlend);
-
-        DWORD val;
-        BOOL opaque;
-        DwmGetColorizationColor(&val, &opaque);
-
-        CLOG(L"=> %x", val);
-    }
-
 private:
+    bool _useUndocumented;
+
+    const int ColorizationParamOrd = 127;
     HRESULT(WINAPI *DwmGetColorizationParameters)
         (DwmColorizationParams *colorParams);
-
 
 };

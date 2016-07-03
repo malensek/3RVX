10bbcc6946830fb2b2928e33a518b61c9522d7b8
Matthew Malensek <matt@malensek.net>
2016-07-02 22:39:25 -0600

    Move undocumented API stuff to a separate method

--- 3RVX/Skin/AccentColor.cpp
+++ 3RVX/Skin/AccentColor.cpp
@@ -1,42 +1,67 @@
 #include "AccentColor.h"
 
 #include <dwmapi.h>
+#include <VersionHelpers.h>
 
 #include "../Logger.h"
 
 AccentColor::AccentColor() {
-    HMODULE dwm = LoadLibrary(L"dwmapi.dll");
-    if (dwm == NULL) {
-        _useUndocumented = false;
-    }
-
-    *(FARPROC *) &DwmGetColorizationParameters
-        = GetProcAddress(dwm, (LPCSTR) ColorizationParamOrd);
-    if (DwmGetColorizationParameters == nullptr) {
-        _useUndocumented = false;
-    }
-
-    DwmColorizationParams params = { 0 };
-    HRESULT hr = DwmGetColorizationParameters(&params);
-    if (FAILED(hr)) {
-        CLOG(L"couldn't get dwm params");
-    }
-
-    CLOG(L"-> %x", params.ColorizationColor);
-    CLOG(L"-> %x", params.ColorizationAfterglow);
-    CLOG(L"-> %d", params.ColorizationColorBalance);
-    CLOG(L"-> %d", params.ColorizationAfterglowBalance);
-    CLOG(L"-> %x", params.ColorizationBlurBalance);
-    CLOG(L"-> %x", params.ColorizationGlassReflectionIntensity);
-    CLOG(L"-> %x", params.ColorizationOpaqueBlend);
-
+    if (IsWindows7OrGreater()) {
     DWORD val;
     BOOL opaque;
     DwmGetColorizationColor(&val, &opaque);
 
     CLOG(L"=> %x", val);
+
+
+    }
+    DWORD dw = GetSysColor(COLOR_ACTIVECAPTION);
+    CLOG(L"-=-> %x", dw);
 }
 
-UINT AccentColor::ColorizationColor() const {
+UINT32 AccentColor::Color() const {
     return 0;
+}
+
+bool AccentColor::HasColor() const {
+    return _hasColor;
+}
+
+/* WARNING: This method uses an undocumented API. May crash in the future. */
+INT64 AccentColor::ColorizationParamColor() {
+    struct DwmColorizationParams {
+        UINT ColorizationColor;
+        UINT ColorizationAfterglow;
+        UINT ColorizationColorBalance;
+        UINT ColorizationAfterglowBalance;
+        UINT ColorizationBlurBalance;
+        UINT ColorizationGlassReflectionIntensity;
+        UINT ColorizationOpaqueBlend;
+    };
+
+    HMODULE dwm = LoadLibrary(L"dwmapi.dll");
+    if (dwm == NULL) {
+        CLOG(L"Could not load DWM library");
+        return -1;
+    }
+
+    HRESULT(WINAPI *DwmGetColorizationParameters)
+        (DwmColorizationParams *colorParams);
+    const int ColorizationParamOrd = 127;
+
+    *(FARPROC *) &DwmGetColorizationParameters
+        = GetProcAddress(dwm, (LPCSTR) ColorizationParamOrd);
+    if (DwmGetColorizationParameters == nullptr) {
+        CLOG(L"Could not retrieve DwmGetColorizationParameters address");
+        return -1;
+    }
+
+    DwmColorizationParams params = { 0 };
+    HRESULT hr = DwmGetColorizationParameters(&params);
+    if (FAILED(hr)) {
+        CLOG(L"Could not retrieve DWM colorization parameters");
+        return -1;
+    }
+
+    return (INT64) params.ColorizationColor;
 }
--- 3RVX/Skin/AccentColor.h
+++ 3RVX/Skin/AccentColor.h
@@ -6,25 +6,29 @@ class AccentColor {
 
 public:
     AccentColor();
-
-    UINT ColorizationColor() const;
-
-private:
-    struct DwmColorizationParams {
-        UINT ColorizationColor;
-        UINT ColorizationAfterglow;
-        UINT ColorizationColorBalance;
-        UINT ColorizationAfterglowBalance;
-        UINT ColorizationBlurBalance;
-        UINT ColorizationGlassReflectionIntensity;
-        UINT ColorizationOpaqueBlend;
-    };
+    UINT32 Color() const;
+    bool HasColor() const;
+    UINT32 FallbackColor() const;
 
 private:
+    bool _hasColor;
     bool _useUndocumented;
 
-    const int ColorizationParamOrd = 127;
-    HRESULT(WINAPI *DwmGetColorizationParameters)
-        (DwmColorizationParams *colorParams);
+    /// <summary>
+    /// Retrieves system colorization parameters, which includes the raw
+    /// colorization color used by DWM for titlebars and other accents. This 
+    /// method returns a different color than the DwmGetColorizationColor()
+    /// function, which doesn't retrieve the actual titlebar color. However,
+    /// it also doesn't return the titlebar color for some themes in Windows 10
+    /// or the automatic colorization based on wallpaper.
+    /// <para>
+    /// This method uses UNDOCUMENTED API calls and may cause horrific crashes
+    /// in the future. You have been warned!
+    /// </para>
+    /// </summary>
+    /// <returns>
+    /// An integer representing the DWM accent color, or -1 if a failure occurs.
+    /// </returns>
+    INT64 ColorizationParamColor();
 
 };

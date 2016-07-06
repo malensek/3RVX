175e4578ca48d676f8a7a05a421b278bf27d1178
Matthew Malensek <matt@malensek.net>
2016-07-06 11:51:04 -0600

    Move constructor to implementation

--- 3RVX/MeterWnd/Meters/HorizontalTile.cpp
+++ 3RVX/MeterWnd/Meters/HorizontalTile.cpp
@@ -3,8 +3,22 @@
 
 #include "HorizontalTile.h"
 
-HorizontalTile::~HorizontalTile()
-{
+HorizontalTile::HorizontalTile(std::wstring bitmapName,
+    int x, int y, int units, bool reverse) :
+Meter(bitmapName, x, y, units),
+_reverse(reverse) {
+    _rect.Width = _bitmap->GetWidth();
+    _rect.Height = _bitmap->GetHeight();
+
+    _texture = new Gdiplus::TextureBrush(_bitmap, Gdiplus::WrapModeTile,
+        0, 0, _rect.Width, _rect.Height);
+    _shiftMat = new Gdiplus::Matrix(1, 0, 0, 1,
+        (float) _rect.X, (float) _rect.Y);
+    _texture->SetTransform(_shiftMat);
+}
+
+
+HorizontalTile::~HorizontalTile() {
     delete _texture;
     delete _shiftMat;
 }

18eb60b6c81dde207dde0b70ab5fee3eefb63fd5
Matthew Malensek <matt@malensek.net>
2016-07-06 11:54:45 -0600

    Move constructor to implementation

--- 3RVX/MeterWnd/Meters/NumberStrip.cpp
+++ 3RVX/MeterWnd/Meters/NumberStrip.cpp
@@ -5,6 +5,16 @@
 
 #include <sstream>
 
+NumberStrip::NumberStrip(std::wstring bitmapName, int x, int y, int units,
+        Gdiplus::StringAlignment align) :
+Meter(bitmapName, x, y, units),
+_align(align) {
+    _charWidth = _bitmap->GetWidth();
+
+    _rect.Width = _bitmap->GetWidth() * 3;
+    _rect.Height = _bitmap->GetHeight() / 10;
+}
+
 void NumberStrip::Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics) {
     int units = CalcUnits();
     int perc = units * (100 / _units);
--- 3RVX/MeterWnd/Meters/NumberStrip.h
+++ 3RVX/MeterWnd/Meters/NumberStrip.h
@@ -9,16 +9,8 @@
 
 class NumberStrip : public Meter {
 public:
-
     NumberStrip(std::wstring bitmapName, int x, int y, int units,
-        Gdiplus::StringAlignment align):
-    Meter(bitmapName, x, y, units),
-    _align(align) {
-        _charWidth = _bitmap->GetWidth();
-
-        _rect.Width = _bitmap->GetWidth() * 3;
-        _rect.Height = _bitmap->GetHeight() / 10;
-    }
+        Gdiplus::StringAlignment align);
 
     virtual void Draw(Gdiplus::Bitmap *buffer, Gdiplus::Graphics *graphics);
     virtual std::wstring ToString();

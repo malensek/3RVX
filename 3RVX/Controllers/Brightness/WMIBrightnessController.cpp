976e3a1d08a99fd096d965b11345da99e9577a4c
Matthew Malensek <matt@malensek.net>
2017-05-29 17:34:40 -0600

    For now, just report monitor IDs

--- 3RVX/Controllers/Brightness/WMIBrightnessController.cpp
+++ 3RVX/Controllers/Brightness/WMIBrightnessController.cpp
@@ -25,6 +25,7 @@ WMIBrightnessController::WMIBrightnessController() {
         return;
     }
 
+    /* Connect to WMI namespace */
     IWbemServices *pSvc = 0;
     hr = pLoc->ConnectServer(
         _bstr_t(L"ROOT\\WMI"),
@@ -43,4 +44,47 @@ WMIBrightnessController::WMIBrightnessController() {
         }
         return;
     }
+
+    /* Set up proxy auth to impersonate the client */
+    hr = CoSetProxyBlanket(
+       pSvc,
+       RPC_C_AUTHN_WINNT,
+       RPC_C_AUTHZ_NONE,
+       NULL,
+       RPC_C_AUTHN_LEVEL_PKT,
+       RPC_C_IMP_LEVEL_IMPERSONATE,
+       NULL,
+       EOAC_NONE);
+
+    if (FAILED(hr)) {
+        CLOG(L"Failed to set proxy blanket: 0x%x", hr);
+        pSvc->Release();
+        pLoc->Release();
+        return;
+    }
+
+    IEnumWbemClassObject* pEnumerator = NULL;
+    hr = pSvc->ExecQuery(
+        bstr_t("WQL"),
+        bstr_t("SELECT * FROM WmiMonitorID"),
+        WBEM_FLAG_RETURN_IMMEDIATELY,
+        NULL,
+        &pEnumerator);
+
+    IWbemClassObject *pclsObj;
+    ULONG uReturn = 0;
+
+    while (pEnumerator) {
+        hr = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+        if (uReturn == 0) {
+            break;
+        }
+
+        VARIANT vtProp;
+        BSTR x;
+        pclsObj->GetObjectText(0, &x);
+        CLOG(L"x-> %s", x);
+        VariantClear(&vtProp);
+    }
+
 }
